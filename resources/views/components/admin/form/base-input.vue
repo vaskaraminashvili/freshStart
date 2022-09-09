87e03451d5407694309af9302f455ad5eb4e8779
vaska <vaska.r02@gmail.com>
2022-09-09 18:36:34 +0400

    wip

---
+++ resources/views/components/admin/form/base-input.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="mb-3 col-xl-4">
+    <label class="form-label" :for="label">{{ label }}</label>
+    <input
+      class="form-control"
+      :id="label"
+      :placeholder="label"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    modelValue: {
+      type: [String, Number],
+      default: "",
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
